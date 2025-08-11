803f690fd3262e8b52e306ccdfae15087e17e0df
dmitriy-zverev <dimazverev716@gmail.com>
2025-08-11 15:47:14 +0300

    add leaf node and unittests for it

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,3 +22,16 @@ class HTMLNode:
     
     def __repr__(self):
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>\nChildren: {self.children}"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Error: LeafNode must have a value")
+        
+        if not self.tag:
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
---
+++ src/test_leafnode.py
@@ -0,0 +1,31 @@
+import unittest
+
+from htmlnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_div(self):
+        node = LeafNode("div", "This is div")
+        self.assertEqual(node.to_html(), "<div>This is div</div>")
+
+    def test_leaf_with_props(self):
+        node = LeafNode("div", "This is div", {"href": "localhost"})
+        self.assertEqual(node.to_html(), "<div href=\"localhost\">This is div</div>")
+    
+    def test_no_children(self):
+        node = LeafNode("div", "This is div")
+        self.assertEqual(node.children, [])
+
+    def test_no_value(self):
+        try: 
+            node = LeafNode("div", None)
+        except ValueError as e:
+            self.assertEqual(str(e), "Error: LeafNode must have a value")
+    
+    def test_no_tag(self):
+        node = LeafNode(None, "Some text without tags")
+        self.assertEqual(node.to_html(), "Some text without tags")
+        
